d551a0df00e0a2d33a429bf4027347bb119f7bb8
Abubakkar A <148679044+abubakkar-dev-code@users.noreply.github.com>
2026-08-27 20:27:12 +0530

    Time: 0 ms (100%), Space: 43 MB (69.22%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str=s.split(" ");
-        return str[str.length-1].length();
+        int count = 0;
+        int i = s.length() - 1;
+
+      
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+       
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
     }
 }
