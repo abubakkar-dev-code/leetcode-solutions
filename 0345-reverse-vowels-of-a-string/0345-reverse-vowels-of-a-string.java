a4ae4a017a47368315f10c16d38d3d2677553d49
Abubakkar A <148679044+abubakkar-dev-code@users.noreply.github.com>
2026-08-28 13:10:19 +0530

    Time: 3 ms (66.15%), Space: 46.7 MB (33.58%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,21 @@
 class Solution {
     public String reverseVowels(String s) {
-        char ch[] = s.toCharArray();
-        int left = 0, right = ch.length - 1;
-
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
         while (left < right) {
-            if (ch[left] == 'a' || ch[left] == 'e' || ch[left] == 'i' || ch[left] == 'o' || ch[left] == 'u'
-                    || ch[left] == 'A' || ch[left] == 'E' || ch[left] == 'I' || ch[left] == 'O' || ch[left] == 'U') {
-                if (ch[right] == 'a' || ch[right] == 'e' || ch[right] == 'i' || ch[right] == 'o' || ch[right] == 'u'
-                        || ch[right] == 'A' || ch[right] == 'E' || ch[right] == 'I' || ch[right] == 'O'
-                        || ch[right] == 'U') {
-                    char temp = ch[left];
-                    ch[left] = ch[right];
-                    ch[right] = temp;
-                    left++;
-                    right--;
-                } else {
-                    right--;
-                }
-            } else {
+            while (left < right && "aeiouAEIOU".indexOf(arr[left]) == -1) {
                 left++;
             }
+            while (left < right && "aeiouAEIOU".indexOf(arr[right]) == -1) {
+                right--;
+            }
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
-        return String.valueOf(ch);
+        return new String(arr);
     }
 }
